8c842e8df8c8f27a51cf13374369b37b44d1a59d
Nikolay-Vasnetsov <kimsofia@protonmail.com>
2021-07-27 18:39:38 +0300

    completed pay method

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, redirect, request
 import sys
+from hashlib import sha256
 
 app = Flask(__name__)
+
+shop_id = 5
+secret_key = "SecretKey01"
+pay_way = "advcash_rub"
+shop_order_id = 4126
+values_currency = {"EUR": 978, "USD": 840, "RUB": 643}
 
 
 @app.route('/')
@@ -11,11 +18,32 @@ def index():
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
-    get_num = request.form['num']
+    amount = request.form['num']
     get_text_area = request.form['text_area']
-    get_value = request.form['select_value']
-    print(get_num, get_text_area, get_value)
-    return redirect('/')
+    currency = request.form['select_value']
+
+    if currency == "EUR":
+        data = pay_method(amount, get_text_area, values_currency['EUR'])
+    elif currency == "USD":
+        data = bill_method()
+
+    return render_template('submit.html', data=data)
+
+
+def bill_method():
+    pass
+
+
+def pay_method(amount, text, currency):
+    str_for_sha256 = str(amount) + ":" + str(currency) + ":" + \
+                     str(shop_id) + ":" + str(shop_order_id) + str(secret_key)
+
+    hash = sha256(str_for_sha256.encode('utf-8')).hexdigest()
+
+    data = {"amount": amount, "currency": currency, "shop_id": shop_id,
+            "shop_order_id": shop_order_id, "sign": hash, "description": text}
+
+    return data
 
 
 if __name__ == '__main__':
