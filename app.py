70af7fb5c0d7bb8a19ceaa12545cc84edfaf2fda
Nikolay-Vasnetsov <kimsofia@protonmail.com>
2021-07-28 16:15:03 +0300

    add logging

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import sys
 from hashlib import sha256
 import requests
 import json
+import time
+import os
 
 app = Flask(__name__)
 
@@ -11,6 +13,8 @@ secret_key = "SecretKey01"
 pay_way = "advcash_rub"
 shop_order_id = 4126
 values_currency = {"EUR": 978, "USD": 840, "RUB": 643}
+
+time_start = time.strftime("%d.%m.%y_%H.%M")
 
 
 @app.route('/')
@@ -25,11 +29,11 @@ def submit():
     currency = request.form['select_value']
 
     if currency == "EUR":
-        data = pay_method(amount, get_text_area, values_currency['EUR'])
+        data = pay_method(amount, get_text_area, values_currency[currency], currency)
         return render_template('pay_method.html', data=data)
 
     elif currency == "USD":
-        data = bill_method(amount, get_text_area, values_currency['USD'], values_currency['RUB'])
+        data = bill_method(amount, get_text_area, values_currency[currency], values_currency['RUB'], currency)
 
         if data['data'] is None:
             return render_template('error.html', data=data)
@@ -37,7 +41,7 @@ def submit():
             return redirect(data['data']['url'])
 
     else:
-        data = invoice_method(amount, values_currency['RUB'], get_text_area)
+        data = invoice_method(amount, values_currency[currency], get_text_area, currency)
 
         if data['data'] is None:
             return render_template('error.html', data=data)
@@ -45,7 +49,7 @@ def submit():
             return render_template("invoice_method.html", data=data)
 
 
-def invoice_method(amount, currency, text):
+def invoice_method(amount, currency, text, v_currency):
     str_for_sha256 = f"{amount}:{currency}:{pay_way}:{shop_id}:{shop_order_id}{secret_key}"
 
     _hash = sha256(str_for_sha256.encode('utf-8')).hexdigest()
@@ -57,11 +61,17 @@ def invoice_method(amount, currency, text):
     info = json.dumps(info)
 
     response = requests.post('https://core.piastrix.com/invoice/create', info, headers=headers)
+    response = response.json()
 
-    return response.json()
+    if response['data'] is None:
+        write_log_file(f'Error: {response["message"]}, {amount} {v_currency}, desc = {text}')
+    else:
+        write_log_file(f'{amount} {v_currency}, id = {response["data"]["id"]}, desc = {text}')
+
+    return response
 
 
-def bill_method(amount, text, payer_currency, shop_currency):
+def bill_method(amount, text, payer_currency, shop_currency, v_currency):
     str_for_sha256 = f"{payer_currency}:{amount}:{shop_currency}:{shop_id}:{shop_order_id}{secret_key}"
 
     _hash = sha256(str_for_sha256.encode('utf-8')).hexdigest()
@@ -74,11 +84,17 @@ def bill_method(amount, text, payer_currency, shop_currency):
     info = json.dumps(info)
 
     response = requests.post('https://core.piastrix.com/bill/create', info, headers=headers)
+    response = response.json()
 
-    return response.json()
+    if response['data'] is None:
+        write_log_file(f'Error: {response["message"]}, {amount} {v_currency}, desc = {text}')
+    else:
+        write_log_file(f'{amount} {v_currency}, id = {response["data"]["id"]}, desc = {text}')
+
+    return response
 
 
-def pay_method(amount, text, currency):
+def pay_method(amount, text, currency, v_currency):
     str_for_sha256 = f"{amount}:{currency}:{shop_id}:{shop_order_id}{secret_key}"
 
     _hash = sha256(str_for_sha256.encode('utf-8')).hexdigest()
@@ -86,7 +102,20 @@ def pay_method(amount, text, currency):
     data = {"amount": amount, "currency": currency, "shop_id": shop_id,
             "shop_order_id": shop_order_id, "sign": _hash, "description": text}
 
+    write_log_file(f'{amount} {v_currency}, desc = {text}')
+
     return data
+
+
+def write_log_file(event):
+    if os.path.exists("log"):
+        pass
+    else:
+        os.mkdir("log")
+
+    with open(f"log/{time_start}_log.txt", "a") as file:
+        log_info = str(time.strftime("[%x %X] ")) + str(event) + "\n"
+        file.write(log_info)
 
 
 if __name__ == '__main__':
